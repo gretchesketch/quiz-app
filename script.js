9a992a9b5e27d4b7139ee4013b481bfbbdc01307
gretchesketch <gretchenjensen92@gmail.com>
2022-03-24 17:30:15 -0400

    Added all the questions and answers to the script file

---
+++ script.js
@@ -0,0 +1,136 @@
+const quizData = [
+    {
+        question: "What is the name of Bender and Fry's apartment building?",
+        a: "MotherBoard of Peace",
+        b: "trick question",
+        c: "Robot Arms",
+        d: "00100100",
+        correct: "c",
+    },
+
+    {
+        question: "What Race is the sworn nemisis of the Niblonians?",
+        a: "The Brain Spawn",
+        b: "Omicronians",
+        c: "Spheroids",
+        d: "Decapodians",
+        correct: "a",
+    },
+
+    {
+        question: "Who is the 'Rocker-Jockey'?",
+        a: "Petunia",
+        b: "trick question",
+        c: "Mom",
+        d: "The Professor",
+        correct: "c",
+    },
+
+    {
+        question: "How many Episodes are in season 6?",
+        a: "20",
+        b: "16",
+        c: "13",
+        d: "4",
+        correct: "d",
+    },
+
+    {
+        question: "What is Dr. Zoidbergs's first name?",
+        a: "Philip",
+        b: "John",
+        c:"Larry",
+        d: "None of the Above.",
+        correct: "b",
+    },
+
+    {
+        question: "Where did fry get his name?",
+        a: "Family name going back all the way to 'Minute Man Philip Fry'.",
+        b: "Philip Hughes",
+        c: "Screw driver",
+        d: "Philip Simms",
+        correct: "c",
+    },
+
+    {
+        question: "Why doesn't anyone use pine trees for X-mas trees anymore?",
+        a: "They were all cut down to make toilet paper during the 50 year squirts.",
+        b: "They were all cut down to grow tall leafy trees when humans where enslaved by giraffes.",
+        c: "They were all scorched during an alien invasion.",
+        d: "All of the Above.",
+        correct: "a",
+    },
+
+    {
+        question: "What concert did Fry wait a thousand years to see?",
+        a: "Butthole Surfers",
+        b: "Beck",
+        c: "Beastie Boys",
+        d: "Neil Diamond",
+        correct: "c",
+    },
+
+    {
+        question: "What is Leela's Surname?",
+        a: "Farnsworth",
+        b: "Wong",
+        c: "Alcazar",
+        d: "Turanga",
+        correct: "d",
+    },
+
+    {
+        question: "What is Fry's brother's name?",
+        a: "Bender",
+        b: "Philip",
+        c: "Adlai",
+        d: "Yancey",
+        correct: "d",
+    },
+
+    {
+        question: "What is the name of the alien news anchor?",
+        a: "Melllvar",
+        b: "Morbo",
+        c: "Kif",
+        d: "Elzar",
+        correct: "b",
+    },
+
+    {
+        question: "Who is the ruler of Omicron persei 8?",
+        a: "Jrrr",
+        b: "Lrrr",
+        c: "Grrl",
+        d: "Ndnd",
+        correct: "b",
+    },
+
+    {
+        question: "Who was the first Emperor of the Moon?",
+        a: "Richard Nixon",
+        b: "Gunter",
+        c: "Al Gore",
+        d: "Philip J. Fry",
+        correct: "c",
+    },
+
+    {
+        question: "What is Bender's last name?",
+        a: "Rodriguez",
+        b: "Bender",
+        c: "Robótica De La Madre",
+        d: "Tijuana",
+        correct: "a",
+    },
+
+    {
+        question: "Where does Zap Branigan work?",
+        a: "The Federation",
+        b: "GOOP",
+        c: "Planet Express",
+        d: "DOOP",
+        correct: "d",
+    },
+]
