ec12bd2e3cccc9801b2dc8cd76c34a405d5a5933
gretchesketch <gretchenjensen92@gmail.com>
2022-03-26 17:45:37 -0400

    Added a funtion to display the questions and answers added a function to deselct upon loading of page added a function to check the correct answer added event listener to submit button

--- script.js
+++ script.js
@@ -136,4 +136,80 @@ const quizData = [
 ];
 
 // getting elements from html
-const quiz = document.getElementById("quiz")
+// const because these will not change
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+// this gives functionality to the questions
+const questionEl = document.getElementById("question");
+//these will give functionality to the answer options
+const a_text = document.getElementById("a_text")
+const b_text = document.getElementById("b_text")
+const c_text = document.getElementById("c_text")
+const d_text = document.getElementById("d_text")
+const submitBtn = document.getElementById("submit")
+
+// let because this will change depending on the question
+// the index starts at 0.
+let currentQuiz= 0;
+//created the score that starts at 0
+let score = 0;
+
+// calling  the function loadQuiz
+loadQuiz();
+
+//decalaring the function for loadquiz/ when it is called this is what will execute.
+function loadQuiz() {
+    //right after we load the quiz one of the choices will already be filled in this will deselect all until a user has clicked an option
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+//this clears the answer choices when you first load the question
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false);
+}
+
+//this loops through the choices and logs the selected answer
+function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+
+    return answer
+}
+
+submitBtn.addEventListener("click", () => {
+    const answer = getSelected()
+
+    //check to see IF ANSWER is EQUAL TO the correct answer that is stated in the array(quizData)for the displayed question(currentQuiz) and look for the correct value
+    if(answer) {
+    if(answer === quizData[currentQuiz].correct) {
+        score++
+    }
+
+    currentQuiz++
+
+    // if there are still questions that have not been answer then proceed to the next question
+    if(currentQuiz < quizData.length) {
+        loadQuiz()
+    } else {
+        quiz.innerHTML = `
+        <h2>You Answered correctly with ${score} /${quizData.length} questions<h2>
+        
+        <button onclick="location.reload()">reload</button>
+        `
+    }
+
+    
+})
